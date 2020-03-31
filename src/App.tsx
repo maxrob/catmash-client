69b48358ef513cd1ea6ac401ba8cdc42c5b42cbb
Maxime Robin <maxime@liberkeys.com>
2020-03-31 19:40:17 +0200

    Prettier files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, NavLink, Switch, Route, Redirect} from 'react-router-dom';
-import './Nav.css';
-import {Battle} from './pages/Battle'
-import {Dashboard} from './pages/Dashboard'
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
+import './Nav.css'
+import { Battle } from './pages/Battle'
+import { Dashboard } from './pages/Dashboard'
 
 function App() {
   return (
@@ -10,18 +16,26 @@ function App() {
       <div>
         <nav className="Nav-header">
           <ul className="Nav-list">
-            <li className="Nav-element"><NavLink to="/battle" activeClassName="Nav-active">Battle</NavLink></li>
-            <li className="Nav-element"><NavLink to={"/dashboard"} activeClassName="Nav-active">Dashboard</NavLink></li>
+            <li className="Nav-element">
+              <NavLink to="/battle" activeClassName="Nav-active">
+                Battle
+              </NavLink>
+            </li>
+            <li className="Nav-element">
+              <NavLink to={'/dashboard'} activeClassName="Nav-active">
+                Dashboard
+              </NavLink>
+            </li>
           </ul>
         </nav>
         <Switch>
           <Route exact path="/battle" component={Battle} />
           <Route exact path="/dashboard" component={Dashboard} />
-          <Redirect to="/battle"/>
+          <Redirect to="/battle" />
         </Switch>
       </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
-import './index.css';
-import App from './App';
-import * as serviceWorker from './library/workers/serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './library/workers/serviceWorker'
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
-);
+)
 
-serviceWorker.unregister();
+serviceWorker.unregister()
